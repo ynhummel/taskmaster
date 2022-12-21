d9d98fe076104cbb0457f9ac976ebffa1c8ed4e8
Yuri Hummel <yuri.hummel@tray.net.br>
2022-12-21 17:33:48 -0300

    add Task struct

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,30 @@
 use std::env;
+use std::fs::File;
 
 fn main() {
-    println!("Welcome to TaskMaster, your personal tasklist.");
-    for argument in env::args() {
-        println!("{}", argument);
+    let action = env::args().nth(1).unwrap_or_default();
+    let mut task_list: Vec<Task> = Vec::new();
+
+    if action == "new" {
+        let name = env::args().nth(2).unwrap();
+        let description = env::args().nth(3).unwrap();
+        let new_task = Task::new(name, description);
+        task_list.push(new_task);
+    }
+
+    for task in task_list {
+        println!("{:?}", task);
+    }
+}
+
+#[derive(Debug)]
+struct Task {
+    name: String,
+    description: String,
+}
+
+impl Task {
+    fn new(name: String, description: String) -> Self {
+        Self { name, description }
     }
 }
